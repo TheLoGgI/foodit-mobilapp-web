1ceaa5254b6973ddf6a3bdbe08c5ee308c11536b
Jonathan-SS <jonathanstensgaard@gmail.com>
2021-12-10 15:39:40 +0100

    Product view and sell product

--- api/index.php
+++ api/index.php
@@ -5,12 +5,35 @@ header("Access-Control-Allow-Headers: *");
 header('Content-Type: application/json');
 
 $API = new API();
-
+if($_GET['action']=="getAllProducts"){
 $res = array(
     "statusText" => 'Success', 
     "status" => 200,
     "data" => $API->products()
 );
+
+}
+
+if($_GET['action']=="getSingleProduct"){
+$res = array(
+    "statusText" => 'Success', 
+    "status" => 200,
+    "data" => $API->singleProduct($_GET['productId'])
+);
+}
+
+if($_GET['action']=="sellProduct"){
+    $API->sellProduct($jsonData->productId,$jsonData->userId);
+    $data=file_get_contents('php://input');
+    $jsonData=json_decode($data);
+$res = array(
+    "statusText" => 'Success', 
+    "status" => 200);
+    
+
+}
+
+
 
 http_response_code(200);
 exit(json_encode($res));
--- views/product.view.php
+++ views/product.view.php
@@ -1,8 +1,8 @@
 <section data-route id="product" class="product-container">
 
-    <div class="product-informationdetails">
+    <div class="product-informationdetails" id="product-informationDetails">
 
-        <div class="product-details">
+        <div class="product-details" id="product-details">
             <div class="product-image-container">
                 <img src="images/products/carrots-200w.jpg" alt="product">
             </div>
@@ -115,16 +115,156 @@
         </div>
         </div>
     </div>
+    <div class="popup-overlay" id="popupOverlay">   
+ <div class="reserve-popup" id="reservePopup">
+    <button class="close-popup" id="closePopup"><img src="icons/close_black_24dp.svg" alt=""></button>
+    <form action=""> </form>
+<h3>Resevere vare</h3>
+<p>Ved klik på nedenstående knap, accepterer du at afhente givne varer indenfor det angivede afhentningstidspunkt.</p>
+<button class="btn btn-primary" id="reserveProduct" >Jeg bekræfter min reservation</button>
+<button class="btn btn-regret">Fortyd</button>
+    </div>
+
+    </div>
+   
     
 </section>
 
 <script>
+    let _currentProduct;
     // Listning for custom event page-change from the spa
     document.addEventListener('page-change', (e) => {
         const productId = e.detail.id
-        const currentProduct = window.data.find(product => Number(product.id) === productId)
-        console.log('currentProduct: ', currentProduct);
-
+        _currentProduct = window.data.find(product => Number(product.id) === productId)
+        console.log('currentProduct: ', _currentProduct);
+fetchProduct(_currentProduct.id)
 
     })
+ async function fetchProduct(productId) {
+const url="http://localhost:3000/api"+"?action=getSingleProduct"+"&productId="+productId;
+            const options = {
+                method: 'get',
+                requestUrl: url,
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+            }
+            const response = await fetch(options.requestUrl, options)
+            
+            if (response.ok) {
+                const requestData = await response.json()
+
+               productData = requestData.data
+                constructElement( productData[0])
+            } else {
+                console.warn('fetch din\'t complete as intended')
+            }
+        }
+
+    function constructElement(data) {
+
+       data.pickupDay=data.pickupDay.substr(8,4)+"-"+data.pickupDay.substr(5,2)+"-"+data.pickupDay.substr(0,4)
+        data.bestBefore=data.bestBefore.substr(8,4)+"-"+data.bestBefore.substr(5,2)+"-"+data.bestBefore.substr(0,4)
+        const container = document.getElementById('product-informationDetails')
+        const htmlString=/*html*/`
+               <div class="product-details" id="product-details">
+            <div class="product-image-container">
+                <img src="${data.image}" alt="product">
+            </div>
+            
+            <div class="infomation-details">
+                <h2>${data.title}</h2>
+                <p class="seller-information">
+                    <span class="link">${data.sellerName}</span>  <br>
+                    ${data.sellerAdress} ${data.postalCode} 
+                </p>
+                <div class="recipe-stars">
+                ${data.sellerRating}/5
+                        <img class="star" src="icons/filledstar.svg" alt="">
+                        <img class="star" src="icons/filledstar.svg" alt="">
+                        <img class="star" src="icons/filledstar.svg" alt="">
+                        <img class="star" src="icons/halfstar.svg" alt="">
+                        <img class="star" src="icons/unfilledstar.svg" alt="">
+                </div>
+                
+            </div>
+    
+        </div>
+        
+        <div class="product-description-details">
+            <h3>Beskrivelse</h3>
+            <p>${data.description}</p>
+        </div>
+        <div class="product-allergens-details">
+            <h3>Allergener</h3>
+            <p>${data.allegenes}</p>
+        </div>
+        <div class="product-bestbefore-details small-details">
+            <h3>Bedst før dato:</h3>
+            <p>${data.bestBefore}</p>
+        </div>
+        <div class="product-pickup-details">
+            <h3>Kan afhentes:</h3>
+            <p>D. ${data.pickupDay} mellem ${data.pickupTime}</p>
+        </div>
+        <form id="reserveProduct">
+            <div class="form-condition">
+            
+                <label for="tradingterms">Jeg acceptere Foodit's handelsbetingelser</label>
+                <input type="checkbox" name="tradingterms" id="tradingterms" required>
+            </div>
+        
+            <input type="submit" value="Resevere vare" class="btn btn-primary">
+        </form>
+        `;
+     container.innerHTML = htmlString
+      document.getElementById('reserveProduct').addEventListener("submit",(e)=>{
+e.preventDefault();
+document.getElementById('popupOverlay').style.display="block";
+    });
+     console.log(data);
+
+    }
+
+
+
+document.getElementById('closePopup').addEventListener("click",(e)=>{
+document.getElementById('popupOverlay').style.display="none";
+    });
+   
+
+async function sellProduct(productId,userId) {
+    const url="http://localhost:3000/api"+"?action=sellProduct";
+            const options = {
+                method: 'post',
+                requestUrl: url,
+                body:JSON.stringify({
+                    'productId':productId,
+                    'userId':userId
+                }),
+                headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+                },
+            }
+            const response = await fetch(options.requestUrl, options)
+            
+            if (response.ok) {
+                console.log("yay")
+                const requestData = await response.json()
+            } else {
+                console.warn('fetch din\'t complete as intended')
+            }
+}
+
+
+document.getElementById('reserveProduct').addEventListener("click",()=>{
+  
+const productId=parseInt(_currentProduct.id);
+sellProduct(productId,4)
+});
+
+
+
 </script>
